f60353a54d5530330dadb03d2ffac039a69d8d82
wangdi <wangdi@loyowo.net>
2024-05-11 18:45:00 +0800

    update gitignore

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const http = require('http'); // 引入http模块
+const fs = require('fs'); // 引入文件系统模块
 const { sendResponse } = require('./js/route')
+
+const jsonDir = './json'
+
 // 创建一个HTTP服务器
 const server = http.createServer((req, res) => {
-  sendResponse(req, res)
+  const files = fs.readdirSync(jsonDir);
+  const jsonFileanmeList = files.map(item => `/${item.replace('.json', '')}`)
+  sendResponse(req, res, jsonFileanmeList)
 });
 
 // 指定服务器监听的端口号
--- js/route.js
+++ js/route.js
@@ -1,6 +1,7 @@
 const fs = require('fs'); // 引入文件系统模块
 const path = require('path');
 
+// 读文件
 const readFile = (filePath) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -13,6 +14,7 @@ const readFile = (filePath) => {
   })
 }
 
+// 写文件
 const writeFile = (filePath, data) => {
   return new Promise((resolve, reject) => {
     fs.writeFile(filePath, data, 'utf8', (err) => {
@@ -25,11 +27,8 @@ const writeFile = (filePath, data) => {
   })
 }
 
-const homeHtml = './index.html'
-const cityHtml = './html/city.html'
-const upsertHtml = './html/upsert.html'
-const jsonDir = './json'
 
+// 读取请求body参数
 async function readRequestBody (req) {
   const data = [];
   // 读取请求体的流
@@ -41,13 +40,14 @@ async function readRequestBody (req) {
   return JSON.parse(buffer.toString());
 }
 
+// 更新数据
 function setRowInData (row, data) {
   for (let i = 0; i < data.length; i++) {
     const item = data[i];
     if (item.cityId === row.cityId) {
       data[i] = row;
       break
-    }else{
+    } else {
       if (item.child) {
         data[i].child = setRowInData(row, item.child)
       }
@@ -56,9 +56,14 @@ function setRowInData (row, data) {
   return data;
 }
 
+// 插入数据
 function insertRowInData (row, data) {
   if (row.upId == 0) {
-    data.push(row)
+    if (data) {
+      data.push(row)
+    } else {
+      data = [row]
+    }
     return data
   }
   for (let i = 0; i < data.length; i++) {
@@ -66,7 +71,7 @@ function insertRowInData (row, data) {
     if (item.cityId === row.upId) {
       data[i].child = [...data[i].child, row];
       break
-    }else{
+    } else {
       if (item.child) {
         data[i].child = insertRowInData(row, item.child)
       }
@@ -75,6 +80,7 @@ function insertRowInData (row, data) {
   return data;
 }
 
+// 删除数据
 function deleteRowFromData (row, data) {
   for (let i = 0; i < data.length; i++) {
     const item = data[i];
@@ -89,7 +95,8 @@ function deleteRowFromData (row, data) {
   return data
 }
 
-const readFileAndResponse = async (res, path) => {
+// 
+const readHtmlAndResponse = async (res, path) => {
   const content = await readFile(path)
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.end(content);
@@ -97,58 +104,97 @@ const readFileAndResponse = async (res, path) => {
 
 const readAndWriteJSONData = async (req, res) => {
   const url = req.url
-  const cityJSONPath = path.join(process.cwd(), `json${url}`)
+  const cityJSONPath = path.join(process.cwd(), url)
   let content = await readFile(cityJSONPath)
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  if (req.method === "GET") {
+    res.end(content);
+    return
+  }
+  const contentJson = JSON.parse(content)
+  const { data } = contentJson
+  const row = await readRequestBody(req);
   if (req.method === 'POST') {
     // 增加
-    const contentJson = JSON.parse(content)
-    const { data } = contentJson
-    const row = await readRequestBody(req);
     contentJson.data = insertRowInData(row, data)
-    await writeFile(cityJSONPath, JSON.stringify(contentJson))
-    content = "{\"msg\":\"操作成功\"}";
   } else if (req.method === 'DELETE') {
     // 删除
-    const contentJson = JSON.parse(content)
-    const { data } = contentJson
-    const row = await readRequestBody(req);
     contentJson.data = deleteRowFromData(row, data)
-    await writeFile(cityJSONPath, JSON.stringify(contentJson))
-    content = "{\"msg\":\"操作成功\"}";
   } else if (req.method === 'PUT') {
     // 修改
-    const contentJson = JSON.parse(content)
-    const { data } = contentJson
-    const row = await readRequestBody(req);
     contentJson.data = setRowInData(row, data)
-    await writeFile(cityJSONPath, JSON.stringify(contentJson))
-    content = "{\"msg\":\"操作成功\"}";
   }
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(content);
+  await writeFile(cityJSONPath, JSON.stringify(contentJson))
+  res.end("{\"msg\":\"操作成功\"}");
 }
 
-const sendResponse = async (req, res) => {
+function setContentTypeByUrl (url) {
+  if (url.endsWith('.js')) {
+    return 'application/javascript'
+  } else if (url.endsWith('.css')) {
+    return 'text/css'
+  } else {
+    return 'text/html'
+  }
+}
+
+// 添加json文件
+async function handleJSONFile (req, res) {
+  const row = await readRequestBody(req)
+  let { fileName } = row
+  if (!fileName.endsWith('.json')) {
+    fileName = fileName + '.json'
+  }
+  const filePath = path.join(process.cwd(), '/json', fileName)
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  if (req.method === 'DELETE') {
+    fs.unlinkSync(filePath)
+    res.end("{\"msg\":\"文件已删除！\"}")
+  } else if (req.method === 'POST') {
+    if (fs.existsSync(filePath)) {
+      res.end("{\"msg\":\"文件已存在！\"}");
+      return
+    }
+    fs.writeFileSync(filePath, JSON.stringify({ data: [] }))
+    res.end("{\"msg\":\"操作成功\"}");
+  }
+}
+
+const frontAssetsDirRegExp = /\/(js|css)\/\**/
+const homeHtml = './index.html'
+const jsonHtml = './html/json.html'
+const upsertHtml = './html/upsert.html'
+const sendResponse = async (req, res, jsonFileanmeList) => {
   const url = req.url;
   try {
     if (url === '/') {
-      readFileAndResponse(res, homeHtml)
+      // 首页页面
+      readHtmlAndResponse(res, homeHtml)
     } else if (url.startsWith('/upsert')) {
-      readFileAndResponse(res, upsertHtml)
-    } else if (/^\/city\-\d+[^\.(json)]*$/.test(url)) {
-      readFileAndResponse(res, cityHtml)
-    } else if (/^\/city-\d+\.json$/.test(url)) {
+      // 添加、修改页面
+      readHtmlAndResponse(res, upsertHtml)
+    } else if (jsonFileanmeList.includes(url)) {
+      // 数据展示页面
+      readHtmlAndResponse(res, jsonHtml)
+    } else if (/^\/json\/.+\.json.*$/.test(url)) {
+      // json文件数据操作，增删查改等
       readAndWriteJSONData(req, res)
-    } else if (url.indexOf('/js/') !== -1 || url.indexOf('/components/') != -1) {
+    } else if (frontAssetsDirRegExp.test(url)) {
+      // 前端资源
       const fp = path.join(process.cwd(), url)
       const content = await readFile(fp)
-      res.writeHead(200, { 'Content-Type': 'text/javascript' });
+      res.writeHead(200, { 'Content-Type': setContentTypeByUrl(url) });
       res.end(content);
-    } else if(url == '/jsondir'){
+    } else if (url == '/jsondir') {
       // 获取 json目录下所有文件名
-      const files = fs.readdirSync(jsonDir);
       res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify(files));
+      res.end(JSON.stringify(jsonFileanmeList));
+    } else if (url == '/handle-json') {
+      // 创建json文件
+      handleJSONFile(req, res)
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end('Not Found');
     }
   } catch (error) {
     res.writeHead(500, { 'Content-Type': 'application/json' });
